3f12b0f888b9377e4d4d72d5bef6162edf2d07b3
Foo JH <foojihaw@hotmail.com>
2025-03-01 19:59:47 +0700

    NEW: working right drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="hHh lpR fFf">
     <q-header elevated>
       <q-toolbar>
         <q-btn
@@ -16,32 +16,42 @@
           PowerDNS Web Admin Lite
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
       </q-toolbar>
+
+      <q-tabs align="left">
+        <q-route-tab to="/page1" label="Page One" />
+        <q-route-tab to="/page2" label="Page Two" />
+        <q-route-tab to="/page3" label="Page Three" />
+      </q-tabs>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label header>
-          Essential Links
-        </q-item-label>
-
-      </q-list>
+    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" overlay bordered>
+      <!-- drawer content -->
     </q-drawer>
 
     <q-page-container>
-      <slot />
+      <router-view />
+      <!-- <slot /> -->
     </q-page-container>
-  </q-layout>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <div>Title</div>
+        </q-toolbar-title>
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-footer>
+</q-layout>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const leftDrawerOpen = ref(false)
-const toggleLeftDrawer = ref (false)
+const rightDrawerOpen = ref(false)
+
+function toggleRightDrawer () {
+  rightDrawerOpen.value = !rightDrawerOpen.value
+}
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page class="q-pl-lg">
+  <q-page class="q-px-lg">
     <div class="text-h6">What is this</div>
     <p class="q-pt-md">
       This is the web management interface for PowerDNS. It's unofficial, barely 
@@ -9,14 +9,32 @@
       Management is purely via PowerDNS API, authenticated by the API key. If 
       this works for you, proceed!
     </p>
-
+    {{ $q.screen.width }}
+      <!-- selection="single"
+      v-model:selected="SelectedRecord"> -->
     <q-table
-      title="Entries"
       :rows="DomainRecords"
       :columns="columns"
-      row-key="name" dense
-      selection="single"
-    />
+      row-key="name" dense>
+      <template v-slot:top="props">
+        <div class="q-table__title">Domain Entries</div>
+
+        <q-space />
+
+        <div >
+          <q-btn flat>New</q-btn>
+        </div>
+      </template>
+
+      <template v-slot:body-cell-actions="props">
+        <q-td :props="props">
+          <q-btn icon="mode_edit" @click="onEdit(props.row)" flat></q-btn>
+          <q-btn v-if="$q.screen.gt.xs" icon="delete" @click="onDelete(props.row)" flat></q-btn>
+        </q-td>
+      </template>
+    </q-table>
+
+    {{  SelectedRecord }}
     <p class="text-h6 q-pt-md">
       Quasar Default Prop Values
     </p>
@@ -28,9 +46,15 @@
 
 <script setup lang="ts">
 import axios from 'axios'
+import { useQuasar } from 'quasar'
 
 const DomainRecords = ref([])
-const columns = [{
+const SelectedRecord = ref([])
+const columns = ref([]) 
+
+const $q = useQuasar()
+console.log($q.screen.width)
+columns.value = $q.screen.width > 412 ? [{
     name: 'rec',
     label: 'Record',
     sortable: true,
@@ -46,8 +70,32 @@ const columns = [{
     label: 'Value',
     sortable: true,
     field: row => row.value,
+  },
+  { 
+    name: 'actions', 
+    label: 'Action',
+    align: 'center' 
+  }
+] : [{
+    name: 'rec',
+    label: 'Record',
+    sortable: true,
+    field: row => row.name,
+    align: 'left',
+  },{
+    name: 'value',
+    label: 'Value',
+    sortable: true,
+    field: row => row.value,
+  },
+  { 
+    name: 'actions', 
+    label: 'Action',
+    align: 'center' 
   }
 ]
+console.log('minimize')
+
 
 try {
   const resp = await axios.get('http://192.168.130.25:8081/api/v1/servers/localhost/zones/kungfoo.local', {
@@ -55,10 +103,13 @@ try {
       'X-API-KEY': 'changeme'
     }
   })
+
+  const MAX_VALUE_LEN = 20
+
   DomainRecords.value = resp.data.rrsets.map((item) => {
     return {
       name: item.name,
-      value: item.records[0].content,
+      value: item.records[0].content.length <= MAX_VALUE_LEN ? item.records[0].content : item.records[0].content.substring(0,MAX_VALUE_LEN - 3) + '...',
       type: item.type,
       ttl: item.ttl,
     }
@@ -74,5 +125,9 @@ try {
   console.log(resp.data.rrsets)
 } catch (err) {
   console.error(err)
+}
+
+function onEdit(row) {
+  console.log(row)
 }
 </script>
